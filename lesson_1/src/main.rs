bd2050b92b9c25bf7b5e1607f47e6d5d920e1244
Patrik Vojta <vojta.patrik@gmail.com>
2025-01-25 09:13:11 +0100

    Lesson 1 homework

---
+++ lesson_1/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let name = String::from("Heisenberg");
+
+    println!("Walter: Say my name.");
+    println!("Declan: {}.", name);
+    println!("Walter: You're goddamn right.");
+}
